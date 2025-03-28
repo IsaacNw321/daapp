d054fd819aa3ba84d0f21693d47d55ec5cc3cbd7
Isaac Pereira <135558153+IsaacNw321@users.noreply.github.com>
2025-03-28 18:07:30 -0400

    ocnfig schem representative

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ npx prisma generate
 
 npx prisma migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --script > baseline.sql
 
-npx prisma migrate resolve --applied 20250307140501_base
+npx prisma migrate resolve --applied 20250328214329_base
 
 npx prisma migrate deploy
